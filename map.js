f2a1ad34b0e0fcd0f5882df8b4d5698065baa7ad
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-05-15 12:02:39 -0700

    step2.1

--- map.js
+++ map.js
@@ -3,3 +3,34 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoicnlvdGFrZW11cmFyeW8iLCJhIjoiY21hcHB0ajZvMDE3eDJpb2FrNzRhNjVkaCJ9.nrDosyQYcCMoQQL4x3RECw';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
+});
+
+map.on('load', async () => {
+    //code
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+      });
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+          'line-color': 'green',
+          'line-width': 3,
+          'line-opacity': 0.4,
+        },
+      });
+  });
