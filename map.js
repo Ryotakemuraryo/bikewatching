172e865ee86caedb4f357421f60abbf0a238aad6
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-05-15 13:47:21 -0700

    step2

--- map.js
+++ map.js
@@ -23,14 +23,32 @@ map.on('load', async () => {
         type: 'geojson',
         data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
       });
+    
     map.addLayer({
-        id: 'bike-lanes',
+        id: 'bike-lanes-boston',
         type: 'line',
         source: 'boston_route',
         paint: {
-          'line-color': 'green',
-          'line-width': 3,
-          'line-opacity': 0.4,
+            'line-color': '#32D400',  // A bright green using hex code
+            'line-width': 5,          // Thicker lines
+            'line-opacity': 0.6       // Slightly less transparent
         },
       });
+
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+      });
+    
+    map.addLayer({
+        id: 'bike-lanes-cambridge',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: {
+            'line-color': '#32D400',  // A bright green using hex code
+            'line-width': 5,          // Thicker lines
+            'line-opacity': 0.6       // Slightly less transparent
+        },
+      });
+    
   });
