30681cb4a5d2bb6a9cc84595d55aff9b808420ec
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-05-15 13:53:08 -0700

    step3.1

--- map.js
+++ map.js
@@ -1,5 +1,6 @@
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
@@ -50,5 +51,17 @@ map.on('load', async () => {
             'line-opacity': 0.6       // Slightly less transparent
         },
       });
+    
+    let jsonData;
+      try {
+        const jsonurl = "https://dsc106.com/labs/lab07/data/bluebikes-stations.json";
+    
+        // Await JSON fetch
+        const jsonData = await d3.json(jsonurl);
+    
+        console.log('Loaded JSON Data:', jsonData); // Log to verify structure
+      } catch (error) {
+        console.error('Error loading JSON:', error); // Handle errors
+      }
     
   });
